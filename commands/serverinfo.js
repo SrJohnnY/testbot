d9470ea482da60d585274a96bb095596f2d3bf34
SrJohnnY <49698409+SrJohnnY@users.noreply.github.com>
2019-04-21 20:48:36 -0300

    Create serverinfo.js

---
+++ commands/serverinfo.js
@@ -0,0 +1,15 @@
+const { RichEmbed }= require("discord.js"); // npm install discord.js
+const moment = require("moment"); // npm install moment
+moment.locale('pt-BR'); // Setar a linguagem do moment
+exports.run = async (client, message, args) => {
+let e = new RichEmbed()
+    .setColor([204, 0, 204])
+    .setTitle(`Informação sobre o servidor.`, client.user.displayAvatarURL)
+    .addField(`Dono`, `<@${message.guild.ownerID}>`)
+    .addField(`Nome do servidor`, `${message.guild.name}`)
+    .addField(`ID Do servidor`, `${message.guild.id}`)
+    .addField(`Criado e*`, `${moment(message.guild.createdAt).format('LLLLL')}`)
+    .addField("Total | Humanos | Bots", `${message.guild.members.size} | ${message.guild.members.filter(member => !member.user.bot).size} | ${message.guild.members.filter(member => member.user.bot).size}`)
+    .addField(`Região do servidor`, `${message.guild.region}`);
+    message.channel.send(e);
+}
